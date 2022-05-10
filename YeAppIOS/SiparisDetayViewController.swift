b66634af2cb84454ca29e707be9cbb15b2523054
ynssasa <yunussasa31@gmail.com>
2022-05-10 11:59:09 -0700

    SiparişDetay and  Siparişlerim UI added.

---
+++ YeAppIOS/SiparisDetayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SiparisDetayViewController.swift
+//  YeAppIOS
+//
+//  Created by ynssasa on 10.05.2022.
+//
+
+import UIKit
+
+class SiparisDetayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
