67b893235a76dfd11ad9209a37e4670a8b330a68
erayterin <erayterin@gmail.com>
2022-05-11 14:25:33 +0300

    Anasayfa Updated.

--- YeAppIOS/UrunCollectionViewCell.swift
+++ YeAppIOS/UrunCollectionViewCell.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
+protocol UrunCollectionProtocol {
+    func sepeteEkle(index : Int)
+}
+
 class UrunCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var urunImg: UIImageView!
     @IBOutlet weak var urunName: UILabel!
-    @IBOutlet weak var urunAzaltImg: UIImageView!
-    @IBOutlet weak var urunArttirImg: UIImageView!
     @IBOutlet weak var urunCount: UILabel!
     @IBOutlet weak var urunPrice: UILabel!
-    @IBOutlet weak var sepeteEkleBtn: UIButton!
+    
+    
+    var urunSayisi: Int = 1
+    
+    var delegate: UrunCollectionProtocol?
+    var index: IndexPath?
+    
+    @IBAction func septEkleBtn(_ sender: Any) {
+        delegate?.sepeteEkle(index: (index?.row)!)
+    }
+    
+    
+    
+    @IBAction func urunAzaltBtn(_ sender: Any) {
+        urunSayisi = (urunCount.text! as NSString).integerValue
+        if urunSayisi != 0
+        {
+            urunSayisi = urunSayisi-1
+        }
+        
+        urunCount.text = urunSayisi.description
+        
+    }
+    @IBAction func urunArttirBtn(_ sender: Any) {
+        urunSayisi = (urunCount.text! as NSString).integerValue
+        urunSayisi = urunSayisi+1
+        urunCount.text = urunSayisi.description
+    }
 }
