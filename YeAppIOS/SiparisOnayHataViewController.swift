627ea5de83f7f88b18fe8e4bf08b1725217199e6
erayterin <erayterin@gmail.com>
2022-05-15 17:49:11 +0300

    Kripto Odeme Sistemi added.

---
+++ YeAppIOS/SiparisOnayHataViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SiparisOnayHataViewController.swift
+//  YeAppIOS
+//
+//  Created by bigsur on 15.05.2022.
+//
+
+import UIKit
+
+class SiparisOnayHataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+
+}
--- YeAppIOS/SiparisOnayiViewController.swift
+++ YeAppIOS/SiparisOnayiViewController.swift
@@ -7,49 +7,17 @@
 
 import UIKit
 
-class SiparisOnayiViewController: UIViewController {
-
+class SiparisOnayiViewController: UIViewController{
     
-    @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var onayText: UILabel!
-    @IBOutlet weak var siparisBilgiText: UILabel!
-    @IBOutlet weak var anasayfayaDonBtn: UIButton!
-    var tikla=true
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        anasayfayaDonBtn.layer.cornerRadius=15
-        anasayfayaDonBtn.layer.masksToBounds=true
-        
-        changeError()
     }
-    
-    func changeView(){
-        if tikla==false {
-            tikla=true
-            changeError()
-        }else{
-            tikla=false
-            changeOK()
-        }
-    }
-    
-    func changeOK() {
-        image.image=UIImage (named:"pay")
-        onayText?.text="Teşekkürler"
-        
-        siparisBilgiText?.text="Siparişiniz onaylandı"
-        
-    }
-    
-    
-    func changeError(){
-        image.image=UIImage (named:"basarisiz_x")
-        onayText?.text="Hata"
-        
-        siparisBilgiText?.text="Siparişiniz onaylanmadı"
-    }
-    
-    
-    
 }
+    
+    
+    
+    
+    
+
