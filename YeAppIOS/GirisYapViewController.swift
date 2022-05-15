cc35af37000b7aa884f9497427f9207fd9b7d849
Eray Terin <erayterin@gmail.com>
2022-05-15 18:23:26 +0300

    Revert "Urun ve kategoriye  firebase eklendi."

--- YeAppIOS/GirisYapViewController.swift
+++ YeAppIOS/GirisYapViewController.swift
@@ -6,13 +6,10 @@
 //
 
 import UIKit
-import Firebase
 
 class GirisYapViewController: UIViewController {
 
     @IBOutlet weak var girisYapBtn: UIButton!
-    @IBOutlet weak var emailText: UITextField!
-    @IBOutlet weak var sifreText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,28 +19,4 @@ class GirisYapViewController: UIViewController {
         
     }
 
-    @IBAction func girisYap(_ sender: Any) {
-       
-        
-        if emailText.text != "" && sifreText.text != ""{
-            Auth.auth().signIn(withEmail: emailText.text!, password: sifreText.text!) { (authdataresult, error) in
-                if error != nil{
-                    self.hataMesaji(titleInput: "HATA", messageInput: error?.localizedDescription ?? "Hata aldınız , tekrar deneyiniz.")
-                }else{
-                    self.performSegue(withIdentifier: "toTabBar", sender: nil)
-                }
-            }
-        }else{
-            hataMesaji(titleInput: "Hata", messageInput: "Email ve Parolayı doğru giriniz.")
-        }
-        
-        
-    }
-    
-    func hataMesaji(titleInput: String,messageInput: String){
-        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
-        let  okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil)
-        alert.addAction(okButton)
-        self.present(alert, animated: true, completion: nil)
-    }
 }
