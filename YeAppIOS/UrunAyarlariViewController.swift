4f94bffcef8a11f855c18f041e31fca82668bb68
ynssasa <yunussasa31@gmail.com>
2022-05-15 07:49:37 -0700

    Urun ve kategoriye  firebase eklendi.

--- YeAppIOS/UrunAyarlariViewController.swift
+++ YeAppIOS/UrunAyarlariViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class UrunAyarlariViewController: UIViewController {
 
@@ -13,6 +14,9 @@ class UrunAyarlariViewController: UIViewController {
     @IBOutlet weak var urunEkleBtn: UIButton!
     @IBOutlet weak var urunGuncelleBtn: UIButton!
     @IBOutlet weak var urunSilBtn: UIButton!
+    @IBOutlet weak var merhabaText: UILabel!
+    let currentUser=Auth.auth().currentUser!.uid
+    let db=Firestore.firestore()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,8 +31,29 @@ class UrunAyarlariViewController: UIViewController {
         
         urunSilBtn.layer.cornerRadius=25
         urunSilBtn.layer.masksToBounds=true
+        getProfilBilgi()
     }
     
+    func getProfilBilgi(){
+        let docRef=db.collection("Users").document(currentUser)
+        
+        docRef.addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                self.hataMesaji(titleInput: "HATA", messageInput: error?.localizedDescription ?? "Hata aldınız , tekrar deneyiniz.")
+            }else{
+                if snapshot != nil {
+                    self.merhabaText.text="Merhaba , "+((snapshot?.get("isim") as? String)!)
+                }
+            }
+        }
+    }
+    
+    func hataMesaji(titleInput: String,messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let  okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 
     
 
